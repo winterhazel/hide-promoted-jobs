6dbb6075263e62a9d14a3fb52e5bf5ad3713a51c
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2023-01-18 09:15:30 -0300

    Switch from Interval to MutationObserver

--- js/hide.js
+++ js/hide.js
@@ -1,31 +1,45 @@
 const JOBS_PATH = '/jobs/search/';
+const PATH_OBSERVER = new MutationObserver((mutations) => checkPathChanges());
+const JOB_LIST_OBSERVER = new MutationObserver((mutations) => run());
 
-let interval;
 let enabled;
+let oldPath;
 
-function initialize() {
-  if (interval) {
-    clearInterval(interval);
+function checkPathChanges() {
+  if (oldPath !== window.location.pathname) {
+    oldPath = window.location.pathname;
+    connectJobListObserver();
   }
+}
 
-  browser.storage.local.get().then((options) => {
-    run(options);
+function run() {
+  const display = enabled ? 'none' : 'initial';
+  const elements = document.querySelectorAll('.t-12.t-normal.t-black--light.job-card-container__footer-item');
+  elements.forEach((element) => {
+    element.parentElement.closest('div > ul > li').style.display = display;
   });
 }
 
-function run(options) {
-  enabled = options.enabled;
-  interval = setInterval(() => {
-    if (!window.location.pathname.startsWith(JOBS_PATH)) {
-      return;
-    }
+function connectPathObserver() {
+  PATH_OBSERVER.disconnect();
+  PATH_OBSERVER.observe(document.body, {childList: true, subtree: true});
+}
 
-    const display = enabled ? 'none' : 'initial';
-    const elements = document.querySelectorAll('.t-12.t-normal.t-black--light.job-card-container__footer-item');
-    elements.forEach((element) => {
-      element.parentElement.closest('div > ul > li').style.display = display;
-    });
-  }, 300);
+function connectJobListObserver() {
+  JOB_LIST_OBSERVER.disconnect();
+  if (window.location.pathname.startsWith(JOBS_PATH)) {
+    const JOB_LIST = document.querySelector('ul.scaffold-layout__list-container');
+    JOB_LIST_OBSERVER.observe(JOB_LIST, {childList: true, subtree: true});
+    run();
+  }
+}
+
+function initialize() {
+  browser.storage.local.get().then((options) => {
+    enabled = options.enabled;
+    connectPathObserver();
+    connectJobListObserver();
+  });
 }
 
 browser.storage.onChanged.addListener(initialize);
