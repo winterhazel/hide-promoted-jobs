713bfcf5855ff6e83de2cdaf17fdfc07115a6a90
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2023-02-01 08:51:45 -0300

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+};
--- ext/js/hide.js
+++ ext/js/hide.js
@@ -15,23 +15,23 @@ function checkPathChanges() {
 
 function run() {
   const display = enabled ? 'none' : 'initial';
-  const li_elements = document.querySelectorAll('li');
-  li_elements.forEach((li) => {
+  const liElements = document.querySelectorAll('li');
+  liElements.forEach((li) => {
     const innerText = li.innerText.trim().toLowerCase();
     if (tags.indexOf(innerText) === -1) {
       return;
     }
 
-    const job_item = li.parentElement.closest('ul > li');
-    if (job_item) {
-      job_item.style.display = display;
+    const jobItem = li.parentElement.closest('ul > li');
+    if (jobItem) {
+      jobItem.style.display = display;
     }
   });
 }
 
 function connectPathObserver() {
   PATH_OBSERVER.disconnect();
-  PATH_OBSERVER.observe(document.body, {childList: true, subtree: true});
+  PATH_OBSERVER.observe(document.body, { childList: true, subtree: true });
 }
 
 function connectJobListObserver() {
@@ -39,7 +39,7 @@ function connectJobListObserver() {
   if (window.location.pathname.startsWith(JOBS_PATH)) {
     const JOB_LIST = document.querySelector('.jobs-search-results-list');
     if (JOB_LIST && tags) {
-      JOB_LIST_OBSERVER.observe(JOB_LIST, {childList: true, subtree: true});
+      JOB_LIST_OBSERVER.observe(JOB_LIST, { childList: true, subtree: true });
       run();
     } else {
       setTimeout(connectJobListObserver, 300);
@@ -49,11 +49,14 @@ function connectJobListObserver() {
 
 function initialize() {
   if (!tags) {
-    let lang_file = browser.runtime.getURL('js/langs.json');
-    fetch(lang_file).then((response) => response.json()).then((json) => {
-      tags = [json[document.documentElement.lang]].flat().map(tag => tag.toLowerCase());
-      console.log(tags);
-    });
+    const langsFile = browser.runtime.getURL('js/langs.json');
+    fetch(langsFile)
+      .then((response) => response.json())
+      .then((json) => {
+        tags = [json[document.documentElement.lang]]
+          .flat()
+          .map((tag) => tag.toLowerCase());
+      });
   }
 
   browser.storage.local.get().then((options) => {
